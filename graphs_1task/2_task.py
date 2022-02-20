11a273fb474179d230a9255ab4e17eb4165207c5
Islamov Sardor <isardor0707@gmail.com>
2022-02-20 23:56:35 +0300

    Изменил построение гистограммы

--- graphs_1task/2_task.py
+++ graphs_1task/2_task.py
@@ -1,7 +1,4 @@
 import matplotlib.pyplot as plt
-
-from random import randint
-import matplotlib.ticker as ticker
 import numpy as np
 
 
@@ -32,41 +29,29 @@ with open(directory, 'r') as file:
     steps = [int(line.rstrip().split()[7]) for line in file.readlines()]
 
 ratio = np.divide(np.multiply(sizex, sizey), dislocations_amount)
-# print(ratio)
-# ratio = sizey
 
-# indexes = sep_indexes(sizex)
-# ratio = separate(ratio, indexes)
-# steps = separate(steps, indexes)
+indexes = sep_indexes(ratio)
+ratio = separate(ratio, indexes)
+steps = separate(steps, indexes)
 
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-# max_y = [0]
-# max_x = [0]
+max_y = [0]
+max_x = [0]
 
-# for i in ratio:
-#     max_x.append(max(i))
-# for i in range(len(steps)):
-#     max_y.append(max(steps[i]))
-#     ax.plot(ratio[i], steps[i])
-    # ax.bar(ratio[i], steps[i], width=5000, align='center', alpha=0.4, color=(0, 0.6, 0.7))
-    # print(max_x)
-# print(ratio)
-# print(steps)
-# ratio = np.arange(0, 50, 1)
-# steps = [randint(0, i) for i in range(50)]
-ax.bar(ratio, steps, width=5000, align='center', alpha=0.4, color=(0, 0.6, 0.7))
-# maxx = max(max_x)
-# maxy = max(max_y)
+for i in ratio:
+    max_x.append(max(i))
+for i in range(len(steps)):
+    max_y.append(max(steps[i]))
+    ax.bar(ratio[i], steps[i], width=5000, align='center', alpha=0.4)
 
-# plt.bar(ratio, steps, align='center', alpha=0.4, color=(0, 0.6, 0.7))
+maxx = max(max_x)
+maxy = max(max_y)
 
-# ax.yaxis.set_major_locator(ticker.MultipleLocator(max(ratio)//5))
-# ax.xaxis.set_major_locator(ticker.MultipleLocator(max(steps)//5))
-plt.xlim(0, 1.05*max(ratio))
-plt.ylim(0, 1.05*max(steps))
+plt.xlim(0, 1.05*maxx)
+plt.ylim(0, 1.05*maxy)
 plt.xlabel('Ratio of area to dislocations amount')
 plt.ylabel('Amount of steps')
 
