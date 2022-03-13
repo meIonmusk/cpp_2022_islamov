7988c7be3a9b31df4e7ed8898266b48d99ab5959
Islamov Sardor <isardor0707@gmail.com>
2022-03-13 15:06:01 +0300

    Добавил вывод в консоль среднего значения скорости, исправил ошибки в формулах.

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -6,14 +6,14 @@ double float_sum(float const psi[], float const pdf[], float const dv, unsigned 
     float sum = 0.f;
     for (unsigned idx = 0; idx < size; idx++)
         sum += psi[idx] * pdf[idx];
-    return sum * dv;
+    return 2 * sum * dv;
 }
 
 float recurrent_sum(float const psi[], float const pdf[], float const dv, unsigned ldx, unsigned rdx){
     auto ldx_ = ldx + (rdx - ldx) / 2;
     return (rdx - ldx)?
-    recurrent_sum(psi, pdf, dv, ldx, ldx_) + recurrent_sum(psi, pdf, dv, ldx_ + 1, rdx) :
-    dv * psi[ldx] * pdf[rdx];
+           (recurrent_sum(psi, pdf, dv, ldx, ldx_) + recurrent_sum(psi, pdf, dv, ldx_ + 1, rdx)) :
+           2 * dv * psi[ldx] * pdf[rdx];
 }
 
 float forward_kahan_sum(float const psi[], float const pdf[], float const dv, unsigned size){
@@ -25,7 +25,7 @@ float forward_kahan_sum(float const psi[], float const pdf[], float const dv, un
         s = (pre_sum - sum) - x;
         sum = pre_sum;
     }
-    return dv * sum;
+    return 2 * dv * sum;
 }
 
 float forward_kahan_sum_fma(float const psi[], float const pdf[], float const dv, unsigned size){
@@ -37,15 +37,14 @@ float forward_kahan_sum_fma(float const psi[], float const pdf[], float const dv
         s = (pre_sum - sum) - x;
         sum = pre_sum;
     }
-    return fma(dv, sum, 0.f);
+    return 2 * fma(dv, sum, 0.f);
 }
 
 double double_sum(float const psi[], float const pdf[], float const dv, unsigned size){
     double sum = 0.;
-    auto ddv = (double)dv;
     for (unsigned idx = 0; idx < size; idx++)
-        sum += (double)psi[idx] * (double)pdf[idx];
-    return sum * ddv;
+        sum += static_cast<double>(psi[idx]) * static_cast<double>(pdf[idx]);
+    return 2 * sum * static_cast<double>(dv);
 }
 
 
@@ -60,15 +59,16 @@ int main() {
     float const dv = 1.f / 128.f;
     for (unsigned idx = 0; idx != n; ++idx) {
         float v = std::fma(dv, static_cast<float>(idx), 0.f);
-        psi[idx] = std::fma(dv, static_cast<float>(idx), 0.f);
-        pdf[idx] = pdf_coeff * std::exp(-psi[idx] * psi[idx]);
+        psi[idx] = v;
+        pdf[idx] = pdf_coeff * std::exp(-psi[idx] * psi[idx] / T);
     }
     std::cout << std::setprecision(10) << std::fixed;
     std::cout << "float sum\n" << float_sum(psi, pdf, dv, n) << '\n';
     std::cout << "recurrent sum\n" << recurrent_sum(psi, pdf, dv, 0, n-1) << '\n';
     std::cout << "forward kahan sum\n" << forward_kahan_sum(psi, pdf, dv, n) << '\n';
-    std::cout << "forward kahan sum with fma using\n" << forward_kahan_sum_fma(psi, pdf, dv, n) << '\n';
+    std::cout << "forward kahan sum with using fma\n" << forward_kahan_sum_fma(psi, pdf, dv, n) << '\n';
     std::cout << "standard sum in double type\n" << double_sum(psi, pdf, dv, n) << '\n';
+    std::cout << "mean velocity value\n" << std::sqrt(T / pif);
 
     return 0;
 }
